be8610c2c71d8e1d4f95825042cf495fb39d0339
DWARAKAKAVYASUDHA <113658909+DWARAKAKAVYASUDHA@users.noreply.github.com>
2022-11-14 23:15:37 +0530

    Delete Project Development Phase/Sprint - 4 directory

--- Project Development Phase/Sprint - 4/static/js/app.js
+++
@@ -1,6 +0,0 @@
-const selectedItem = document
-  .getElementById('sidebarCollapse')
-  .addEventListener('click', (e) => {
-    const ele = document.getElementById('sidebar');
-    ele.classList.toggle('active');
-  });
